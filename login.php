f5949b129c334a976fc7e7f9219e03dec69509fe
Mike Ratsimbazafy <ratsimbazafy.louis@gmail.com>
2018-05-18 18:29:28 -0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,121 @@
+<?php
+// Include config file
+require_once 'config.php';
+ 
+// Define variables and initialize with empty values
+$username = $password = "";
+$username_err = $password_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = 'Please enter username.';
+    } else{
+        $username = trim($_POST["username"]);
+    }
+    
+    // Check if password is empty
+    if(empty(trim($_POST['password']))){
+        $password_err = 'Please enter your password.';
+    } else{
+        $password = trim($_POST['password']);
+    }
+    
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT username, password FROM users WHERE username = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+            $param_username = $username;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Store result
+                mysqli_stmt_store_result($stmt);
+                
+                // Check if username exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            /* Password is correct, so start a new session and
+                            save the username to the session */
+                            //At the time of login we'll verify the given password with the password hash stored in the database using the PHP password_verify() function
+                            session_start();
+                            $_SESSION['username'] = $username;      
+                            header("location: home.php");
+                        } else{
+                            // Display an error message if password is not valid
+                            $password_err = 'The password you entered was not valid.';
+                        }
+                    }
+                } else{
+                    // Display an error message if username doesn't exist
+                    $username_err = 'No account found with that username.';
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Takeluft</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <link href="css/signin.css" rel="stylesheet">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+
+<body class="background-image">
+        <div id="form-box" class="text-center">
+            <div class="form-signin">
+                <img class="mb-4" src="avatar/default_avatar.png" alt="" width="72" height="72">
+                <h1 class="h3 mb-3 font-weight-normal">To see the wonders, please sign in!</h1>
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                    <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                        <input type="text" name="username"class="form-control" value="<?php echo $username; ?>" placeholder="Username" required autofocus>
+                        <span class="help-block"><?php echo $username_err; ?></span>
+                    </div>    
+                    <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                        <input type="password" name="password" class="form-control" placeholder="Password" required>
+                        <span class="help-block"><?php echo $password_err; ?></span>
+                    </div>
+                    <div class="form-group">
+                      <label>
+                        <input type="checkbox" value="remember-me"> Remember me
+                      </label>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-lg btn-info btn-block" value="Sign me in">
+                    </div>
+                    <p align="left">Lost your password?<a href="#"> Reset it</a>!</p>
+                    <p align="left">Don't have an account? <a href="register.php">Sign up now</a>.</p>
+                </form>
+                <p class="mt-5 mb-3 text-muted" style="padding-top: 28px;">&copy; M@gic-Mike 2018-2019</p>
+            </div>  
+        </div>  
+
+</body>
+</html>
